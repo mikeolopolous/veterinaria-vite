b03a693657de4e61b9aed127bc3fe58e6ad7376c
Miguel Lopez <ing.miguellv@gmail.com>
2022-09-26 12:59:57 -0500

    add mockups

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 const Header = () => {
   return (
-    <>
-      <h1 className="font-bold">Header</h1>
-    </>
+      <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
+        Seguimiento pacientes {''}
+        <span className="text-indigo-600">Veterinaria</span>
+      </h1>
   )
 }
 
