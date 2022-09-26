5452be13e0fdb6bc4fe25ce1ac6752f9efc32bfb
Miguel Lopez <ing.miguellv@gmail.com>
2022-09-26 13:04:34 -0500

    add form

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,97 @@
 const Formulario = () => {
   return (
-    <>Formulario</>
+    <div className="md:w-1/2 lg:w-2/5">
+      <h2 className="font-black text-3xl text-center">
+        Seguimiento pacientes
+      </h2>
+
+      <p className="text-lg mt-5 text-center mb-10">
+        Añade pacientes y {''}
+        <span className="text-indigo-600 font-bold">adminístralos</span>
+      </p>
+
+      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+        <div className="mb-5">
+          <label
+            htmlFor="mascota"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Nombre mascota:
+          </label>
+          <input
+            id="mascota"
+            type="text"
+            placeholder="Nombre de la mascota"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label
+            htmlFor="propietario"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Propietario:
+          </label>
+          <input
+            id="propietario"
+            type="text"
+            placeholder="Nombre del propietario"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label
+            htmlFor="contacto"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Email:
+          </label>
+          <input
+            id="contacto"
+            type="email"
+            placeholder="ejemplo@ejemplo.com"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label
+            htmlFor="fechaRegistro"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Fecha registro:
+          </label>
+          <input
+            id="fechaRegistro"
+            type="date"
+            className="border-2 w-full p-2 mt-2 rounded-md"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label
+            htmlFor="sintomas"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Sintomas:
+          </label>
+          <textarea
+            id="sintomas"
+            placeholder="Describe los síntomas"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+
+        <input
+          type="submit"
+          className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
+          value="Agregar paciente"
+        />
+      </form>
+
+    </div>
   )
 }
 
